980f2ab9b35e736dc03b9cc2724c1f114f399079
Abbey Sparrow <abbey@khrome.net>
2022-10-15 16:08:52 -0600

    adding UMD support

--- extend-interface.js
+++ extend-interface.js
@@ -1,18 +1,31 @@
-let extendify = function(ext, supr, cls){
-    var copy = supr || function(){};
-    //var copy = function(){ return orig.apply(this, arguments) };
-    Object.keys(cls.prototype).forEach(function(key){
-        copy.prototype[key] = cls.prototype[key];
-    });
-    Object.keys(ext).forEach(function(key){
-        copy.prototype[key] = ext[key];
-    });
-    copy.extend = function(ext, supr){
-        return extendify(ext, supr, copy);
-    };
-    return copy;
-}
-
-extendify.default = extendify;
-
-module.exports = extendify;
+(function (root, factory){
+    if(typeof define === 'function' && define.amd){
+        // AMD. Register as an anonymous module.
+        define([], factory);
+    }else if (typeof module === 'object' && module.exports){
+        module.exports = factory();
+    }else{
+        // Browser globals (root is window)
+        root.extendInterface = factory();
+    }
+}(this, function(){
+    
+    let extendify = function(ext, supr, cls){
+        var copy = supr || function(){};
+        //var copy = function(){ return orig.apply(this, arguments) };
+        Object.keys(cls.prototype).forEach(function(key){
+            copy.prototype[key] = cls.prototype[key];
+        });
+        Object.keys(ext).forEach(function(key){
+            copy.prototype[key] = ext[key];
+        });
+        copy.extend = function(ext, supr){
+            return extendify(ext, supr, copy);
+        };
+        return copy;
+    }
+    
+    extendify.default = extendify;
+    
+    return extendify;
+}));
